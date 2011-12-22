647a40c6edab0882ef4fc8877e1c89489ab4d2e7
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-12-21 20:10:23 -0800

    Define bonk semantics completely via tests.

--- test/bonk_test.rb
+++ test/bonk_test.rb
@@ -18,5 +18,16 @@ class BonkTest < MiniTest::Unit::TestCase
 
     assert_equal @object, temp_obj
   end
+
+  def test_bonk_returns_the_value_of_the_block
+    assert_equal :funky_monkey, @object.bonk{ :funky_monkey }
+  end
+
+  def test_bonk_does_replace_its_callee
+    obj_id = @object.object_id
+    @object.bonk{ |o| o = :a_funkier_monkey_than_ever }
+
+    assert_equal obj_id, @object.object_id
+  end
 end
 
